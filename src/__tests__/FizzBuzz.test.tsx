1c56bf0cb7b6132a51e334b2320e3ddfa7cef709
syuji-higa <syuji.higa@gmail.com>
2020-11-13 15:22:06 +0900

    テストに ToDo 追加

--- src/__tests__/FizzBuzz.test.tsx
+++ src/__tests__/FizzBuzz.test.tsx
@@ -4,6 +4,16 @@ import Index from '~/pages/index'
 
 afterEach(cleanup)
 
-describe('Index page', (): void => {
-  it.todo('Index TODO')
+// 要件
+describe('Fizz Buzz 整列と変換規則を扱う実装', () => {
+  // 仕様
+  it.todo(
+    '入力エリアに 3 と 5 の倍数を入力したら、出力エリアに "fizzbuzz" を表示する'
+  )
+  // 仕様
+  it.todo('入力エリアに 3 の倍数を入力したら、出力エリアに "fizz" を表示する')
+  // 仕様
+  it.todo('入力エリアに 5 の倍数を入力したら、出力エリアに "buzz" を表示する')
+  // 仕様
+  it.todo('その他の数字を入力したら、そのまま出力エリアに表示する')
 })
