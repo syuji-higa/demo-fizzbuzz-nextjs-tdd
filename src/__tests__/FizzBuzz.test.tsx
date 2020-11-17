76e67e0882b24c6de3eae2ca431735c5e15ba807
syuji-higa <syuji.higa@gmail.com>
2020-11-17 13:21:56 +0900

    feat: 完成

--- src/__tests__/FizzBuzz.test.tsx
+++ src/__tests__/FizzBuzz.test.tsx
@@ -1,15 +1,80 @@
 import * as React from 'react'
-import { render, cleanup, fireEvent } from '@testing-library/react'
+import { render, cleanup, fireEvent, act } from '@testing-library/react'
 import Index from '~/pages/index'
 
 afterEach(cleanup)
 
-describe('入力された値を fizzbuzz 問題の答えとして表示する', (): void => {
-  it.todo('入力エリアに数字を入力したら、文字列として出力エリアに表示する')
+// 仕様
+describe('Fizz Buzz 整列と変換規則を扱う実装', () => {
+  // 要件
+  describe('入力エリアに 3 と 5 の倍数を入力したら、出力エリアに "fizzbuzz" を表示する', () => {
+    // テスト内容
+    it('入力エリアに "15" を入力したら、出力エリアに "fizzbuzz" を表示する', () => {
+      // 準備（arrange）
+      const { getByTestId } = render(<Index />)
+      const inputElement = getByTestId('input')
+      const outputElement = getByTestId('output')
+      // 実行（act）
+      fireEvent.change(inputElement, { target: { value: '15' } })
+      // 検証（assert）
+      expect(outputElement.textContent).toBe('fizzbuzz')
+    })
+  })
 
-  it.todo('入力エリアに 3 の倍数を入力したら、"fizz" を出力エリアに表示する')
+  // 要件
+  describe('入力エリアに 3 の倍数を入力したら、出力エリアに "fizz" を表示する', () => {
+    // テスト内容
+    it('入力エリアに "3" を入力したら、出力エリアに "fizz" を表示する', () => {
+      // 準備（arrange）
+      const { getByTestId } = render(<Index />)
+      const inputElement = getByTestId('input')
+      const outputElement = getByTestId('output')
+      // 実行（act）
+      fireEvent.change(inputElement, { target: { value: '3' } })
+      // 検証（assert）
+      expect(outputElement.textContent).toBe('fizz')
+    })
+  })
 
-  it.todo('入力エリアに 5 の倍数を入力したら、"buzz" を出力エリアに表示する')
+  // 要件
+  describe('入力エリアに 5 の倍数を入力したら、出力エリアに "buzz" を表示する', () => {
+    // テスト内容
+    it('入力エリアに "5" を入力したら、出力エリアに "buzz" を表示する', () => {
+      // 準備（arrange）
+      const { getByTestId } = render(<Index />)
+      const inputElement = getByTestId('input')
+      const outputElement = getByTestId('output')
+      // 実行（act）
+      fireEvent.change(inputElement, { target: { value: '5' } })
+      // 検証（assert）
+      expect(outputElement.textContent).toBe('buzz')
+    })
+  })
 
-  it.todo('入力エリアに 3 と 5 の倍数を入力したら、"fizzbuzz" を出力エリアに表示する')
+  // 要件
+  describe('その他の数字を入力したら、そのまま出力エリアに表示する', () => {
+    // テスト内容
+    it('入力エリアに "1" を入力したら、出力エリアに "1" 表示する', () => {
+      // 準備（arrange）
+      const { getByTestId } = render(<Index />)
+      const inputElement = getByTestId('input')
+      const outputElement = getByTestId('output')
+      // 実行（act）
+      fireEvent.change(inputElement, { target: { value: '1' } })
+      // 検証（assert）
+      expect(outputElement.textContent).toBe('1')
+    })
+
+    // テスト内容（三角測量：関連するテストをすべて書いたら消す）
+    it('入力エリアに "2" を入力したら、出力エリアに "2" 表示する', () => {
+      // 準備（arrange）
+      const { getByTestId } = render(<Index />)
+      const inputElement = getByTestId('input')
+      const outputElement = getByTestId('output')
+      // 実行（act）
+      fireEvent.change(inputElement, { target: { value: '2' } })
+      // 検証（assert）
+      expect(outputElement.textContent).toBe('2')
+    })
+  })
 })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,29 @@ import { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [num, setNum] = useState('')
+
+  const convertFizzbuzz = (e) => {
+    const num = Number(e.target.value)
+    if(!(num % (3 * 5))) {
+      setNum('fizzbuzz')
+      return
+    }
+    if(!(num % 3)) {
+      setNum('fizz')
+      return
+    }
+    if(!(num % 5)) {
+      setNum('buzz')
+      return
+    }
+    setNum(e.target.value)
+  }
+
   return (
     <div className={styles.container}>
-
+      <p><input type="text" data-testid="input" onChange={convertFizzbuzz} /></p>
+      <p data-testid="output">{num}</p>
     </div>
   )
 }
